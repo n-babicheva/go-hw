6b991e00063583325cf01a77baa7939d06f7bf69
Nadya Babicheva <nadya_stepanova_1992@mail.ru>
2024-07-26 23:37:38 +0300

    Вывод текста введенного до enter

--- hw03_calculator/main.go
+++ hw03_calculator/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	// TODO: write your code here
+	var action string
+	var a, b int
+	fmt.Scan(&action, &a, &b)
+	if action == "+" {
+		fmt.Println(a + b)
+	} else if action == "-" {
+		fmt.Println(a - b)
+	} else if action == "*" {
+		fmt.Println(a * b)
+	} else if action == "/" {
+		if b == 0 {
+			fmt.Println("Error: b mustn't be zero")
+		} else {
+			fmt.Println(a / b)
+		}
+	} else {
+		fmt.Println("Error: enter correct data")
+	}
+
 }
